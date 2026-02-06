bb515aae28faf5fb5cf9d319ed7289708390122b
jackabhi11 <jackabhi11@gmail.comgit config --global user.email jackabhi11@gmail.com>
2026-02-06 20:49:59 +0530

    Todays problem was from math

---
+++ DSAPractice/src/Math/VolumeOfRoom.java
@@ -0,0 +1,15 @@
+package Math;
+
+public class VolumeOfRoom {
+	
+	public static void main(String args[]) {
+		int height = 10;
+		int width = 12;
+		int length = 15;
+		
+		int volume = height * width * length;
+		
+		System.out.println("Volume of the Room: " + volume + "meter cube");
+	}
+
+}
